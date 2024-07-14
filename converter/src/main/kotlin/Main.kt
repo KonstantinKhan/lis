17187e5236ec0e0275d7ff5a0c8bf09610fdd635
KonstantinKhan <khan366kos@gmail.com>
2024-07-14 11:10:25 +0500

    removed comment lines

--- converter/src/main/kotlin/Main.kt
+++ converter/src/main/kotlin/Main.kt
@@ -18,60 +18,42 @@ fun main() {
     }
     val entries = workbook.entries(0, 0)
 
-    val groupList = entries.map { entry -> entry.groups.toList() }.distinct()
-//    val uniqueGroups = groupList.flatten().filter { it.first.isNotBlank() && it.second.isNotBlank() }.distinct()
-    val uniqueGroups = groupList.map { it.first() }.distinct()
-//    println(uniqueGroups)
-//        .flatten().filter { it.first.isNotBlank() && it.second.isNotBlank() }.distinct()
-
-//    groupList.forEach { println(it) }
-//    uniqueGroups.forEach { println(it) }
-
-    fun innerGroups(parentId: String, allGroups: List<List<Pair<String, String>>>): List<Group> {
-        val result = allGroups
-            .asSequence()
-            .filter { a ->
-                a.any { b -> b.second == parentId }
-            }
-            .toList()
-            .asSequence()
-            .filter { a -> a.size >= a.indexOf(a.find { pair -> pair.second == parentId }) + 2 }
-            .map { a ->
-                a[a.indexOf(a.find { pair -> pair.second == parentId }) + 1]
-            }
-            .distinct().filter { a -> a.first.isNotBlank() && a.second.isNotBlank() }
-            .map { pair ->
-                Group(
-                    groupName = pair.first,
-                    groupId = pair.second,
-                    groups = innerGroups(pair.second, groupList),
-                    instances = listOf()
-                )
-            }
-            .toList()
-        return result
-    }
-
-    val groups: List<Group> = uniqueGroups.map { pair ->
-        Group(
-            groupName = pair.first,
-            groupId = pair.second,
-            groups = innerGroups(pair.second, groupList),
-            instances = listOf()
-        )
-    }
-
-//    val groups = Group(
-//        groupName = uniqueGroups[4].first,
-//        groupId = uniqueGroups[4].second,
-//        groups = innerGroups(uniqueGroups[4].second, groupList),
-//        instances = listOf()
-//    )
-
-//    groups.forEach { group -> println(group) }
-
-
-//    groups.forEach { group -> println(group) }
+//    val groupList = entries.map { entry -> entry.groups.toList() }.distinct()
+//    val uniqueGroups = groupList.map { it.first() }.distinct()
+//
+//    fun innerGroups(parentId: String, allGroups: List<List<Pair<String, String>>>): List<Group> {
+//        val result = allGroups
+//            .asSequence()
+//            .filter { a ->
+//                a.any { b -> b.second == parentId }
+//            }
+//            .toList()
+//            .asSequence()
+//            .filter { a -> a.size >= a.indexOf(a.find { pair -> pair.second == parentId }) + 2 }
+//            .map { a ->
+//                a[a.indexOf(a.find { pair -> pair.second == parentId }) + 1]
+//            }
+//            .distinct().filter { a -> a.first.isNotBlank() && a.second.isNotBlank() }
+//            .map { pair ->
+//                Group(
+//                    groupName = pair.first,
+//                    groupId = pair.second,
+//                    groups = innerGroups(pair.second, groupList),
+//                    instances = listOf()
+//                )
+//            }
+//            .toList()
+//        return result
+//    }
+//
+//    val groups: List<Group> = uniqueGroups.map { pair ->
+//        Group(
+//            groupName = pair.first,
+//            groupId = pair.second,
+//            groups = innerGroups(pair.second, groupList),
+//            instances = listOf()
+//        )
+//    }
 
     val result = with(entries) {
         reference {
@@ -81,8 +63,6 @@ fun main() {
     }
 
     val json = Json { prettyPrint = true }
-
-//    println(json.encodeToString(result))
 
     File("file.json").printWriter()
         .use { out ->
